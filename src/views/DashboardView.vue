3d127d00b844e36a436279d01da4a98145ea7746
Victor Nieto <vmnieto2@gmail.com>
2025-03-29 13:38:56 -0500

    se realiza un ajuste visual en el dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
     <LayoutView>
-      <div class="card-container">
+      <!-- <div class="card-container">
         <div class="card" v-for="(modulo, index) in modulos" :key="index">
           <div class="card-header">
             <img :src="`/${modulo.icon}`" :alt=modulo.icon>
@@ -13,7 +13,23 @@
             <button class="primary-button login-button" @click="go_action(modulo.action)">Ir</button>
           </div>
         </div>
-      </div>
+      </div> -->
+      <div class="container-dashboard">
+            <div class="form-container-dashboard">
+                <div class="card-container-dashboard">
+                    <div class="card-dashboard" v-for="(modulo, index) in modulos" :key="index" @click="go_action(modulo.action)">
+                      <div class="card-header">
+                        <img :src="`/${modulo.icon}`" :alt=modulo.icon>
+                        <h5 class="card-title">{{modulo.name}}</h5>
+                      </div>
+                      <div class="card-body">
+                        <p class="card-text">{{ modulo.description }}</p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
   </LayoutView>
 </template>
 
@@ -49,43 +65,66 @@ html {
   font-family: "DM Sans", serif;
 }
 
-.card-container {
-  width: 100%;
-  display: flex;
+.container-dashboard {
+    max-width: 100%;
+    margin: 0 auto;
+    padding: 0;
 }
 
-.card {
-  flex: 100%;
-  max-width: 200px;
-  margin: 10px;
-  border: 3px solid black;
-  display: flex;
-  flex-direction: column;
+.form-container-dashboard {
+    background-color: #ffffff;
+    padding: 24px;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    margin-top: 24px;
 }
 
-.card .card-header {
+.card-container-dashboard {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    gap: 20px;
+    margin-top: 20px;
+}
+
+.card-dashboard {
+    background: #f2f2f2;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s;
+}
+
+.card-dashboard:hover {
+    transform: scale(1.05);
+    cursor: pointer;
+}
+
+.card-dashboard .card-header {
   display: flex;
   flex-direction: column;
   align-items: center;
   padding-bottom: 4px;
 }
 
-.card .card-header img {
+.card-dashboard .card-header img {
   width: 5.0rem
 }
 
-.card .card-header h5 {
+.card-dashboard .card-header h5 {
   font-size: 1.2rem;
 }
 
-.card .card-body {
+.card-dashboard .card-body {
   padding-left: 5px;
   flex-grow: 1;
   flex-shrink: 1;
 }
 
-.card .card-footer {
-  background-color: rgba(240, 240, 240, 0.932);
+.card-dashboard .card-body .card-text{
+  text-align: center;
+}
+
+.card-dashboard .card-footer {
   padding: 10px;
   display: flex;
   justify-content: center;
